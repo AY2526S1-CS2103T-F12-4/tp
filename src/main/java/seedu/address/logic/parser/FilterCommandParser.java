e97a2d692304ed53dc5ebd7908b2c12d421800ca
rahul <rahul.vijay.0406@gmail.com>
2025-10-22 22:06:18 +0800

    Add skeleton code for filter command

---
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -0,0 +1,9 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FilterCommand;
+
+public class FilterCommandParser implements Parser<FilterCommand>{
+    public FilterCommand parse(String args) {
+        return null;
+    }
+}
