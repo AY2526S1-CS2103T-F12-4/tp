9a2660ae0837157c098336e5e0e999203b604bb4
rahul <rahul.vijay.0406@gmail.com>
2025-10-23 17:43:01 +0800

    Add skeleton code for command and parser

---
+++ src/main/java/seedu/address/logic/commands/FindMedicineCommand.java
@@ -0,0 +1,9 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+public class FindMedicineCommand extends Command {
+    public CommandResult execute(Model model) {
+        return null;
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/FindMedicineCommandParser.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FindMedicineCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class FindMedicineCommandParser implements Parser<FindMedicineCommand> {
+    @Override
+    public FindMedicineCommand parse(String args) throws ParseException {
+        return null;
+    }
+}
