be50c4be1e6917bfd362e22ea24929b41bad42be
arnav-jhajharia-2 <jhajhariaarnav@gmail.com>
2025-11-04 12:47:02 +0800

    Name: disallow slashes; remove d/o and s/o exceptions

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,10 +10,10 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names cannot be blank and can take any alphanumeric characters, spaces and any "
-                    + "of these special characters: comma, at, hyphen, and slash";
+        "Names cannot be blank and can contain alphanumeric characters, spaces, commas, "
+        + "the at symbol (@), and hyphens. Slashes are not allowed.";
 
-    public static final String VALIDATION_REGEX = "^(?!\\s*$)[a-zA-Z0-9\\s\\/\\-@,]+$";
+    public static final String VALIDATION_REGEX = "^(?!\\s*$)[A-Za-z0-9 @,\\-]+$";
 
     public final String fullName;
 
